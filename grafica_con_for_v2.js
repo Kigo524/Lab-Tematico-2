31ef5607624e45ea64873fc7d7b44f8ab5d6bc25
Kigo524 <kigocode@gmail.com>
2026-05-16 18:36:18 -0600

    Implementa la lógica para agrupar y dibujar barras por edad y altura en la gráfica, añadiendo etiquetas para los ejes.

--- grafica_con_for_v2.js
+++ grafica_con_for_v2.js
@@ -33,5 +33,68 @@ function setup(){
         .domain([0, 2])
         .range([lineaBase, 30]) //para que a altura sea desde la base hsata 30 pixeles antes del tope
     
-    
+    //AQUI LA LOGICA PARA AGRUPAR POR EDADES. uso un ordenamiento de js
+    datos.sort((a,b) => a.edad, b.edad); //entiendo que los ordena de forma ascendente 
+    //https://d3js.org/d3-array/sort#sort
+
+    //y ahora para ordenar esos datos por grupo de forma visual
+    let xActual = 50; //posicion de la primera barra de la izquierda
+    const anchoBarra = 35;
+    const espacioEntreBarras = 4;
+    const espacioEntreGrupos = 45;
+    let edadAnterior = null; //variable para controlar cuando poner los espacios de edades distintas
+
+    //ciclo
+    for(let i=0; i<datos.length; i++){
+        const d= datos[i];
+        /*como se supone que ya esta ordenado, solo busco que cambie la edad para dar el salto*/
+        if(edadAnterior !== null && d.edad !== edadAnterior){
+            xActual += espacioEntreGrupos; //aqui se hace el salto de grupo
+        }
+
+        //eso dibuja una barra
+        dibujarRectangulo(d, xActual, anchoBarra, escalaY, color);
+
+        //ahora se avanza el espacio entre barras
+        xActual += anchoBarra + espacioEntreBarras;
+
+        //y guardo la edad actual para compararla en la siguiente vuelta
+        edadAnterior = d.edad;
+    }
+
+    svg.append("text")
+        .attr("x", width_espacio - 110)
+        .attr("y", baseGrafica + 35)
+        .style("font-family", "sans-serif")
+        .style("font-weight", "bold")
+        .text("EJE: EDAD");
+
+    svg.append("text")
+        .attr("x", width_espacio - 110)
+        .attr("y", 40)
+        .style("font-family", "sans-serif")
+        .style("font-weight", "bold")
+        .text("EJE: ALTURA");
 }
+
+function dibujarRectangulo(dato, x, ancho, escalaY, color, lineaBase){
+    svg .append("rect")
+        .attr("x", x)
+        .attr("y", escalaY(dato.estatura)) //dice donde empieza el rectangulo de arriba a abajo
+        .attr("width", ancho)
+        .attr("height", lineaBase - escalaY(dato.estatura)) //queda: la base menos el tope superior para que se ajuste
+        .attr("fill", color.fill)
+        .attr("stroke", color.stroke)
+        .attr("stroke-width", "2")
+
+    //para el texto de la estatura arriba de cada barra individual
+    svg .append("text")
+        .attr("x", x + (ancho/2)) //que sea la mitad de cada barra
+        .attr("y", escalaY(dato.estatura) -6) //se coloca 6 pixeles arriba de la barra
+        .attr("text-anchor", "middle")
+        .style("font-family", "sans-serif")
+        .style("font-size", "11px")
+        .text(dato.estatura);
+}
+
+setup();
